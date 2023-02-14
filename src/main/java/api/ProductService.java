0907640f35d0df149df0e8a1710630fdaf2a550e
Aitor Rodriguez Gallardo <Aitor.gallardo.14@gmail.com>
2023-02-14 15:13:54 +0100

    Initial commit

---
+++ src/main/java/api/ProductService.java
@@ -0,0 +1,10 @@
+package api;
+
+import java.util.Set;
+
+public class ProductService implements IProductService {
+    @Override
+    public Set<Product> findAll() {
+        return null;
+    }
+}
